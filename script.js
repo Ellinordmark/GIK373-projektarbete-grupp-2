267f0ff1978405d059ffb97eadbc0bd554ff3c7a
Ellinordmark <109726891+Ellinordmark@users.noreply.github.com>
2024-05-22 15:43:19 +0200

    To top button

--- script.js
+++ script.js
@@ -137,6 +137,28 @@ const tags = document.querySelector(".text-animation");
 
 observer.observe(tags);
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // ------------------- MEDIA QUERIES -------------------- //
 
 function myFunction(x) {
